fead5683811949bb0bf77f1aef072c087619f37e
Dasago250 <Dasago250@gmail.com>
2023-02-07 11:19:51 -0500

    Round of 9 turns and each turn a different symbol

--- script.js
+++ script.js
@@ -1,29 +1,54 @@
-const gameboard = (() => {
+const gameBoard = (() => {
   const board = [ '', '', '', '', '', '', '', '', '']
 
-  return {board};
+  const displayBoard = () => {
+    const boardCells = document.querySelectorAll('.cell')
+    boardCells.forEach((cell, index) => {
+      cell.textContent = board[index];
+    });
+  };
+
+  const addMark = (symbol) => {
+    const cells = document.querySelectorAll('.cell');
+      cells.forEach((cell, index) => {
+        cell.addEventListener('click', () => {
+          if (numberOfSpaces() > 0) {
+            board[index] = symbol
+            displayBoard();
+            game.changeTurn(numberOfSpaces());            
+          }
+        });
+      });
+  }
+
+  const numberOfSpaces = () => {
+    let spaces = 0
+    for (const item in board) {
+      if (board[item] === '') {
+        spaces++;
+      }
+    }
+    return spaces;
+  }
+
+  return {board, addMark, numberOfSpaces};
 })();
 
 const players = (playerNum, symbol) => {
-
   return {playerNum, symbol};
 };
 
-const displayBoard = (() => {
-  const boardCells = document.querySelectorAll('.cell')
-  boardCells.forEach((cell, index) => {
-    cell.textContent = gameboard.board[index];
-  });
-});
+const game = (() => {
+  const player1 = players('Player 1' , 'X');
+  const player2 = players('Player 2' , 'O');
+  const changeTurn = (signal) => {
+    if (signal % 2 !== 0) {
+      gameBoard.addMark(player1.symbol);
+    }else if(signal % 2 === 0){
+      gameBoard.addMark(player2.symbol);
+    }
+  }
+  return {changeTurn}
+})();
 
-const addMark = (() => {
-  const cells = document.querySelectorAll('.cell');
-  cells.forEach((cell, index) => {
-    cell.addEventListener('click', () => {
-      gameboard.board[index] = 'X'
-      displayBoard();
-    });
-  });
-})
-
-addMark()
+game.changeTurn(gameBoard.numberOfSpaces());
